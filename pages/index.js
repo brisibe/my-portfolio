a4f01dd8618cb9708bed09df5d726dcec77a6aab
brisibe <43288228+jbrisibe10@users.noreply.github.com>
2021-04-08 00:47:50 +0100

    bug fix

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
 import Head from "next/head";
-import Link from "next/link";
+import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import { ProjectCard } from "../components/ProjectCard/ProjectCard";
-import { Page } from "../components/Layout/Page";
+import { Nav } from "../Navbar/Nav";
+import styles from "../../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <Page>
+    <div className={styles.wrapper}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="A fullstack software developer portfolio built on Next.js"
+        ></meta>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0"
+        ></meta>
+        <title>Brisibe's Software Developer Portfolio</title>
+      </Head>
+
+      <Nav />
+
       <div className={styles.content_wrapper}>
         <div className={styles.hero_section}>
           <div className={styles.hero_section_info}>
@@ -121,6 +136,6 @@ export default function Home() {
           </a>
         </p>
       </footer>
-    </Page>
+    </div>
   );
 }
